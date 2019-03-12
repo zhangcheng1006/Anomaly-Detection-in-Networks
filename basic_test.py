6a89c09239a2688caddd225f6baff76b3e297d36
svenhsia <svenhsia@gmail.com>
2019-03-12 21:43:27 +0100

    add basic test comments

--- basic_test.py
+++ basic_test.py
@@ -1,8 +1,20 @@
+"""This module performs basic test on a given graph 
+by assigning each node a basic score according to its 
+GAW subject to a random distribution (null hypothesis)."""
 import networkx as nx
 import numpy as np
 from scipy.stats import norm
 
 def GAW(weights, metric='gaw'):
+    """Computes the GAW statistics of a set of edge weights.
+    Parameters:
+    ------
+        weights: array-like, a set of weights
+        metric: str, default `gaw`, options `gaw`, `gaw10` and `gaw20`. The type of GAW to compute.
+    
+    Returns:
+        gaw: float, the GAW statistics.
+    """
     ordered_weights = sorted(weights, reverse=True)
     proportion = len(weights)
     if metric in ['gaw10', 'GAW10']:
@@ -14,6 +26,18 @@ def GAW(weights, metric='gaw'):
     return gaw
 
 def monte_carlo_sampler(n_edges, all_weights, n_samples=10000, metric='gaw'):
+    """A Monte-Carlo simulator to generate random GAW statistics.
+    Parameters:
+    -----------
+        n_edges: int, the degree (in-degree + out-degree) of the node
+        all_weights: all edge weights appearing in the graph from which the simulator choose weights from
+        n_samples: int, default `10000`. The number of simulations to repreat.
+        metric: str, default `gaw`, options `gaw`, `gaw10` and `gaw20`. The type of GAW to compute.
+    
+    Returns:
+    --------
+        (avg, std): a tuple consisting of the mean and standard deviation of the simulated GAWs.
+    """
     gaws = []
     for _ in range(n_samples):
         sampled_weights = np.random.choice(all_weights, n_edges, replace=True)
@@ -22,9 +46,31 @@ def monte_carlo_sampler(n_edges, all_weights, n_samples=10000, metric='gaw'):
     return np.mean(gaws), np.std(gaws)
 
 def compute_p(value, avg, std):
+    """Computes the p-value of a specific value to a Gaussian distribution.
+    Parameters:
+    -----------
+        value: float, the value whose p-value to be computed
+        avg: the mean of the Gaussian distribution
+        std: the standard deviation of the Gaussian distribution
+    
+    Returns:
+    --------
+        p_value: float, the computed p-value
+    """
     return 1 - norm.cdf(value, loc=avg, scale=std)
 
 def compute_node_basic(edges, shared_distribution, metric='gaw'):
+    """Computes the basic score of a node.
+    Parameters:
+    -----------
+        edges: array-like, the edge weights of the node
+        shared_distribution: dict, the null Gaussian distributions for different node degrees
+        metric: str, default `gaw`, options `gaw`, `gaw10` and `gaw20`. The type of GAW to compute.
+
+    Returns:
+    --------
+        score: float, the basic score of the node
+    """
     node_gaw = GAW(edges, metric)
     n_edges = len(edges)
     mean_gaw, std_gaw = shared_distribution[n_edges]
@@ -36,7 +82,15 @@ def compute_node_basic(edges, shared_distribution, metric='gaw'):
     return score
 
 def basic_test(G, metric='gaw'):
-    """Compute the basic scores of nodes in the graph, and attach the scores to nodes as an attribute `basic_score`.
+    """Compute the basic scores of all nodes in the graph, and attach the scores to nodes as an attribute `basic_score`.
+    Parameters:
+    -----------
+        G: a networkx.Graph object, the graph to compute basic scores
+        metric: str, default `gaw`, options `gaw`, `gaw10` and `gaw20`. The type of GAW to compute.
+    
+    Returns:
+    --------
+        scores: dict with nodes as keys and nodes' scores as values
     """
     all_weights = list(nx.get_edge_attributes(G, 'weight').values())
     degrees = set(G.degrees.values())
