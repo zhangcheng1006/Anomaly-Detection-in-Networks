9e1ec6d732fa7e614095493e5cf614ebbf6799d0
Unknown <cheng.zhang@polytechnique.edu>
2019-03-18 17:55:56 +0100

    add NetEMD

--- NetEMD.py
+++ NetEMD.py
@@ -28,15 +28,14 @@ utils.chooseCRANmirror(ind=1) # select the first mirror in the list
 #     utils.install_packages(StrVector(names_to_install))
 
 import rpy2.robjects as robjects
-# install = robjects.r['''devtools::install_github("alan-turing-institute/network-comparison")''']
 
 netdist = rpackages.importr('netdist')
 
 import numpy as np
-import scipy.stats as stats
 import community
 from utils import to_undirected_graph, generate_null_model
 from com_detection import get_partition
+from scipy.stats import norm
 
 motif_dict = {  ((0, 2), (1, 0), (1, 0)): 4, 
                 ((0, 1), (0, 1), (2, 0)): 5, 
@@ -52,11 +51,19 @@ motif_dict = {  ((0, 2), (1, 0), (1, 0)): 4,
                 ((1, 2), (2, 1), (2, 2)): 15, 
                 ((2, 2), (2, 2), (2, 2)): 16}
 
-def strength_stats(graph):
-    for node in graph.nodes():
-        graph.node[node]['in_strength'] = sum([data[2] for data in graph.in_edges(node, data='weight')])
-        graph.node[node]['out_strength'] = sum([data[2] for data in graph.out_edges(node, data='weight')])
-        graph.node[node]['in_out_strength'] = graph.node[node]['in_strength'] + graph.node[node]['out_strength']
+stats = ['in_strength', 'out_strength', 'in_out_strength', 'motif_4_stat', 'motif_5_stat', 'motif_6_stat', 
+         'motif_7_stat', 'motif_8_stat', 'motif_9_stat', 'motif_10_stat', 'motif_11_stat', 'motif_12_stat', 
+         'motif_13_stat', 'motif_14_stat', 'motif_15_stat', 'motif_16_stat', 'upper_1_netemd', 'upper_2_netemd', 
+         'upper_3_netemd', 'upper_4_netemd', 'upper_5_netemd', 'lower_1_netemd', 'lower_2_netemd', 'lower_3_netemd', 
+         'lower_4_netemd', 'lower_5_netemd', 'comb_1_netemd', 'comb_2_netemd', 'comb_3_netemd', 'comb_4_netemd', 
+         'comb_5_netemd', 'rw_1_netemd', 'rw_2_netemd', 'rw_3_netemd', 'rw_4_netemd', 'rw_5_netemd']
+
+def strength_stats(graph, communities):
+    for sub_g in communities:
+        for node in sub_g.nodes():
+            graph.node[node]['in_strength'] = sum([data[2] for data in sub_g.in_edges(node, data='weight')])
+            graph.node[node]['out_strength'] = sum([data[2] for data in sub_g.out_edges(node, data='weight')])
+            graph.node[node]['in_out_strength'] = graph.node[node]['in_strength'] + graph.node[node]['out_strength']
     return graph
 
 def get_motif(g, motif_dict):
@@ -66,20 +73,21 @@ def get_motif(g, motif_dict):
     motif = motif_dict.get(tuple(res))
     return motif
 
-def motif_stats(graph, num_motif=13):
-    all_motif_stats = np.zeros(num_motif)
-    for node in graph.nodes():
-        hop1 = set(graph.in_edges(node, data='weight'))|set(graph.out_edges(node, data='weight'))
-        for _, a in hop1:
-            hop2 = set(graph.in_edges(a, data='weight'))|set(graph.out_edges(a, data='weight'))|hop1
-            for _, b in hop2:
-                if a < b:
-                    sub_g = graph.subgraph([node, a, b])
-                    motif = get_motif(sub_g)
-                    stat = np.prod([data[2] for data in sub_g.edges.data('weight', default=1)])
-                    all_motif_stats[motif-4] += stat
-        for i in range(num_motif):
-            graph.node[node]['motif_'+str(i+1)+'_stat'] = all_motif_stats[i]
+def motif_stats(graph, communities, num_motif=13):
+    for sub_g in communities:
+        all_motif_stats = np.zeros(num_motif)
+        for node in sub_g.nodes():
+            hop1 = set(sub_g.in_edges(node, data='weight'))|set(sub_g.out_edges(node, data='weight'))
+            for _, a in hop1:
+                hop2 = set(sub_g.in_edges(a, data='weight'))|set(sub_g.out_edges(a, data='weight'))|hop1
+                for _, b in hop2:
+                    if a < b:
+                        tri_sub_g = sub_g.subgraph([node, a, b])
+                        motif = get_motif(tri_sub_g)
+                        stat = np.prod([data[2] for data in tri_sub_g.edges.data('weight', default=1)])
+                        all_motif_stats[motif-4] += stat
+            for i in range(num_motif):
+                graph.node[node]['motif_'+str(i+4)+'_stat'] = all_motif_stats[i]
     return graph
 
 def compute_hist(g, stat):
@@ -92,17 +100,74 @@ def compute_NetEMD(g1, g2, stat):
     dhist1 = netdist.dhist(h1[1], h1[0])
     dhist2 = netdist.dhist(h2[1], h2[0])
     dist = netdist.net_emd(dhist1, dhist2)
+    return dist
 
 def assign_NetEMD_score(graph, num_references=15, num_samples=500):
-    communities = get_partition(graph)
+    communities = get_partition(graph).values()
+    num_communities = len(communities)
+    references = generate_null_model(num_models=num_references, min_size=5)
+    samples = generate_null_model(num_models=num_samples, min_size=5)
+    graph = strength_stats(graph, communities)
+    graph = motif_stats(graph, communities)
 
+    #TODO
 
+    y_M = np.zeros((num_samples, 36))
+    for i in range(36):
+        for j, sam_g in enumerate(samples):
+            dists = []
+            for ref_g in references:
+                dists.append(compute_NetEMD(sam_g, ref_g, stats[i]))
+            dists.remove(min(dists))
+            dists.remove(max(dists))
+            y_M[j, i] = np.mean(dists)
 
+    y = np.zeros((num_communities, 36))
+    for i in range(36):
+        for j, sub_g in enumerate(communities):
+            dists = []
+            for ref_g in references:
+                dists.append(compute_NetEMD(sub_g, ref_g, stats[i]))
+            dists.remove(min(dists))
+            dists.remove(max(dists))
+            y[j, i] = np.mean(dists)
 
-# x = np.linspace(0, 10, 1000)
-# y = np.linspace(1, 11, 1000)
+            p_value = sum(y_M[:, i] > y[j, i]) / num_samples
+            if p_value >= 0.05:
+                for node in sub_g.nodes():
+                    graph.node[node]['NetEMD_score_1_'+str(i+1)] = 0
+                    graph.node[node]['NetEMD_score_2_'+str(i+1)] = 0
+            else:
+                T_stats = np.reshape([[data[0], data[1]] for data in sub_g.nodes.data(stats[i], default=0)], (-1, 2))
+                mean = np.mean(T_stats[:, 1])
+                std = np.std(T_stats[:, 1])
+                for k in range(T_stats.shape[0]):
+                    node = T_stats[k, 0]
+                    value = T_stats[k, 1]
+                    criterion = abs(value - mean) / std
+                    if criterion < 2:
+                        graph.node[node]['NetEMD_score_1_'+str(i+1)] = 0
+                    else:
+                        graph.node[node]['NetEMD_score_1_'+str(i+1)] = criterion
+                indexes = np.argsort(T_stats[:, 1])
+                bottom = int(T_stats.shape[0] * 0.95) + 1
+                for idx in range(bottom):
+                    node = T_stats[indexes[idx], 0]
+                    graph.node[node]['NetEMD_score_2_'+str(i+1)] = 0
+                for idx in range(bottom, T_stats.shape[0]):
+                    node = T_stats[indexes[idx], 0]
+                    graph.node[node]['NetEMD_score_2_'+str(i+1)] = norm.ppf(1 - p_value)
 
-# u1 = robjects.FloatVector(stats.norm.pdf(x))
-# u2 = robjects.FloatVector(stats.expon.pdf(y))
+    for i in range(4):
+        for node in graph.nodes():
+            value1 = 0
+            value2 = 0
+            for j in range(5):
+                value1 += graph.node[node]['NetEMD_score_1_'+str(17 + i*5 + j)]
+                value2 += graph.node[node]['NetEMD_score_2_'+str(17 + i*5 + j)]
+            graph.node[node]['Compact_NetEMD_score_1_'+str(i+1)] = value1
+            graph.node[node]['Compact_NetEMD_score_2_'+str(i+1)] = value2
 
-# print(netdist.net_emd(u1, u2))
+    return graph
+                
+
